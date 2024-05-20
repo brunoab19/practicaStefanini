33d6cc9d5210af252ee97dd5892812575f7f9c45
brunoab19 <bruno.aburto01@gmail.com>
2024-05-20 11:01:17 -0500

    actualizacion del pom y creacion de service

---
+++ bbva/src/main/java/com/practica/bbva/service/PersonasService.java
@@ -0,0 +1,38 @@
+package com.practica.bbva.service;
+
+import com.practica.bbva.model.Personas;
+import com.practica.bbva.repository.PersonasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonasService {
+
+    @Autowired
+    private PersonasRepository personasRepository;
+
+    public List<Personas> findAll() {
+        return personasRepository.findAll();
+    }
+
+    public Optional<Personas> findById(Long id) {
+        return personasRepository.findById(id);
+    }
+
+    public Personas save(Personas personas) {
+        return personasRepository.save(personas);
+    }
+
+    public void deleteById(Long id) {
+        personasRepository.deleteById(id);
+    }
+
+    public Optional<Personas> findByDni(String dni) {
+        return personasRepository.findByDni(dni);
+    }
+}
+
+
